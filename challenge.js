46bceb5604b0b3b3092d16d1427764a5606f73a2
Ayaan Khalif <ayaankhalif@Ayaans-MacBook-Air-4.local>
2023-05-27 20:36:11 +0300

    Football points -Done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,14 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+function calculatePoints(wins, draws, losses) {
+    // Calculate total points
+    let totalPoints = wins * 3 + draws * 1 + losses * 0;
+  
+    // Log the total points
+    console.log("Total points: " + totalPoints);
+  }
+  
+  // Example usage
+  calculatePoints(10, 7, 4);
